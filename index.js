67c503fabba3fbe55cde8d4ca7c5a2c09986d76f
Owais Ahmed <ovaisahmed43@gmail.com>
2019-10-28 00:09:17 +0500

    FIX- Unsupported event onGestureHandlerStateChange

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler'; // https://github.com/kmagiera/react-native-gesture-handler/issues/320
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
